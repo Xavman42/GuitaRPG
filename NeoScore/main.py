81fbcabcb59aa10a6ebc9456e2dc66ea719b6f5b
Xavman42 <xavieredavenport@hotmail.com>
2023-01-08 11:46:40 -0600

    Graphic update at intersections

--- NeoScore/main.py
+++ NeoScore/main.py
@@ -2,16 +2,20 @@ from map import *
 from Plevel import *
 from HUD import *
 from multiprocessing import Process, Value, Manager
+import os
+import pathlib
 
 
 def refresh_func(func_time: float):
     global my_angle, my_move_dur, new_move, my_x_move_rate, my_y_move_rate, reference_time, rotate_dist, \
-        my_next_point, my_staves, network, my_scene_changed, my_last_index, hud_last_index, hud_share_dict
+        my_next_point, my_staves, network, my_scene_changed, my_last_index, hud_last_index, hud_share_dict, \
+        my_top_layer_assets
     move_rate = 120
     if new_move:
         my_angle, distance, my_next_point, my_staves, my_scene_changed, my_last_index, share_dict = \
             calculate_trajectory(my_point, my_last_point, my_last_index, possible_paths, my_staves, my_network,
                                  my_level_dict, my_xp_dict, my_network_points)
+        my_top_layer_assets = redo_top_layer_assets(my_top_layer_assets)
         my_move_dur = distance.base_value / move_rate
         my_x_move_rate = cos(radians(my_angle)) * move_rate
         my_y_move_rate = sin(radians(my_angle)) * move_rate
@@ -41,11 +45,11 @@ def camera_rotate_refresh_func(real_time: float) -> neoscore.RefreshFuncResult:
     global reference_time, my_prev_angle, my_scene_changed
     t = real_time - reference_time
     rotation_time = 1
-    zoom = 5+5*(sin(t*pi/rotation_time))
+    zoom = 5 + 5 * (sin(t * pi / rotation_time))
     neoscore.set_viewport_scale(zoom)
     if abs(my_angle - my_prev_angle) < 180:
         neoscore.set_viewport_rotation(-my_prev_angle - t * (my_angle - my_prev_angle) / rotation_time)
-    elif (my_angle-my_prev_angle) < 0:
+    elif (my_angle - my_prev_angle) < 0:
         neoscore.set_viewport_rotation(-my_prev_angle + t * rotate_dist / rotation_time)
     else:
         neoscore.set_viewport_rotation(-my_prev_angle + t * rotate_dist / rotation_time)
@@ -81,6 +85,7 @@ def camera_forward_refresh_func(real_time: float) -> neoscore.RefreshFuncResult:
 
 def hud_process_func(last_index, current_index, share_dict):
     global ref_current_index
+
     def hud_refresh_func(func_time: float):
         global ref_current_index
         if ref_current_index != last_index.value:
@@ -90,29 +95,49 @@ def hud_process_func(last_index, current_index, share_dict):
             for key, value in share_dict.items():
                 text_dict[key][0].text = str(key) + ": " + str(value)
 
-
     ref_current_index = -1
     neoscore.setup()
     initialize_map()
-    my_network_points, possible_paths, my_network = make_network()
-    mini_map_network = my_network
+    network_points, possible_paths, network = make_network()
+    mini_map_network = network
     mini_staff = []
     mini_scale = 9
     for coord in mini_map_network:
-        mini_staff.append(Path.straight_line(Point(Unit(75+coord[0]/mini_scale), Unit(1+coord[1]/mini_scale)), None,
-                                             Point(Unit(coord[2]/mini_scale-coord[0]/mini_scale),
-                                                   Unit(coord[3]/mini_scale-coord[1]/mini_scale)), None,
-                                             Brush.no_brush(), Pen("#000000", Unit(0.5))))
+        mini_staff.append(
+            Path.straight_line(Point(Unit(75 + coord[0] / mini_scale), Unit(1 + coord[1] / mini_scale)), None,
+                               Point(Unit(coord[2] / mini_scale - coord[0] / mini_scale),
+                                     Unit(coord[3] / mini_scale - coord[1] / mini_scale)), None,
+                               Brush.no_brush(), Pen("#000000", Unit(0.5))))
     mini_staff[0].pen = Pen("#2a51ee", Unit(3))
     hud_elements = []
     hud_elements, text_dict = make_level_text(hud_elements)
     x, y = neoscore.get_viewport_center_pos()
-    hud_elements = set_hud_coordinates(hud_elements, x-Unit(125), y+Unit(75))
+    hud_elements = set_hud_coordinates(hud_elements, x - Unit(125), y + Unit(75))
 
     neoscore.set_viewport_scale(5)
     neoscore.set_refresh_func(hud_refresh_func, 5)
     neoscore.show(display_page_geometry=False, auto_viewport_interaction_enabled=False,
                   min_window_size=(1920, 360), max_window_size=(1920, 360))
+
+
+def load_assets():
+    cwd = os.getcwd()
+    disk = pathlib.Path(cwd + "/Assets/circle")
+    top_layer_assets = []
+    for i in my_network_points:
+        top_layer_assets.append(Image((Unit((i[0]-42.5)), Unit((i[1]-12.5))), None, disk, scale=1/8))
+    return top_layer_assets
+
+
+def redo_top_layer_assets(top_layer_assets):
+    cwd = os.getcwd()
+    disk = pathlib.Path(cwd + "/Assets/circle")
+    for i in top_layer_assets:
+        i.remove()
+    top_layer_assets = []
+    for i in my_network_points:
+        top_layer_assets.append(Image((Unit((i[0] - 42.5)), Unit((i[1] - 12.5))), None, disk, scale=1 / 8))
+    return top_layer_assets
 
 
 if __name__ == '__main__':
@@ -139,6 +164,7 @@ if __name__ == '__main__':
     for idx, coord in enumerate(my_network):
         my_staves = np.append(my_staves, make_map_segment(coord[0], coord[1], coord[2], coord[3]))
 
+    my_top_layer_assets = load_assets()
     my_point = 0
     my_current_index = 0
     my_next_point = 0
