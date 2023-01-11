cf15807bcf9d62061efe7005d27cab4ad9d219de
Xavman42 <xavieredavenport@hotmail.com>
2023-01-11 09:44:24 -0600

    Particle.py is functional. Messages can be sent to Supercollider with ease

--- NeoScore/Particle.py
+++ NeoScore/Particle.py
@@ -1,22 +1,29 @@
 import pyOSC3
 
 
-def send_particle(client, region):
+def send_particle(client, region, level):
     # This sends a message to SuperCollider.
     msg = pyOSC3.OSCMessage()
     msg.setAddress("/print")
     msg.clearData()
     if region == "scrape":
-        name = "scratchy_string"
-    elif region == "B":
-        name = "plinky_wood"
-    elif region == "C":
-        name = "cavernous_thunk"
+        name = "scrape"
+    elif region == "perc":
+        name = "perc"
+    elif region == "tamb":
+        name = "tamb"
+    elif region == "bart":
+        name = "bart"
+    elif region == "none":
+        name = "scrape"
+        level = 1
     elif region == "Null":
         name = "Null"
     else:
         print("what region is this!?")
+    msg.append("area")
     msg.append(name)
+    msg.append(level)
     try:
         client.send(msg)
     except ValueError:
@@ -37,3 +44,4 @@ def update_effect(client, effect_name):
 if __name__ == '__main__':
     my_client = pyOSC3.OSCClient()
     my_client.connect(('127.0.0.1', 57120))
+    send_particle(my_client, "none", 2)
